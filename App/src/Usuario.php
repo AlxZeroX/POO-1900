892a06892746f685cb323255a7a5131e651b6bcf
FredyX <feamador@unah.hn>
2019-09-09 23:49:41 -0600

    Clases Backend

--- App/src/Usuario.php
+++ App/src/Usuario.php
@@ -9,18 +9,40 @@ class Usuario
     private $apellido;
     private $contrasena;
     private $correoElectronico;
+    private $edad;
 
     public function __construct($correoElectronico,$contrasena)
     {
             $this->correoElectronico = $correoElectronico;
             $this->contrasena = password_hash($contrasena,PASSWORD_DEFAULT);
     }
-    public function seNombre($nombre)
+    public function setNombre($nombre)
     {
         $this->nombre = $nombre;
     }
     public function getNombre()
     {
         return $this->nombre;
+    }
+    public function getApellido(){
+        return $this->apellido;
+    }
+    public function setApellido( $apellido){
+        $this->apellido = $apellido;
+    }
+    public function setEdad($edad){
+        $this->edad = $edad;
+    }
+    public function getEdad(){
+        return $this->edad;
+    }
+    public function setContrasena($contrasena){
+        $this->contrasena = $contrasena;
+    }
+    public function setCorreoElectronico($correoElectronico){
+        $this->correoElectronico = $correoElectronico;
+    }
+    public function getCorreoElectronico(){
+        return$this->correoElectronico;
     }
 }
